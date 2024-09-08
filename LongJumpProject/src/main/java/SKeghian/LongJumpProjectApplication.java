542d0dd0bfb267adab889f739560bcf458cd2161
Sarah Keghian <keghian.sarah@gmail.com>
2024-09-08 20:32:39 +0200

    Initial commit
    Gradle Project initialisation

---
+++ LongJumpProject/src/main/java/SKeghian/LongJumpProjectApplication.java
@@ -0,0 +1,13 @@
+package SKeghian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongJumpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongJumpProjectApplication.class, args);
+	}
+
+}
---
+++ LongJumpProject/src/test/java/SKeghian/LongJumpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package SKeghian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongJumpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
